910c37c525f87f0d05173717aaccd10cd3e2b1be
rokamu623 <r.okamura.061@ms.saitama-u.ac.jp>
2023-02-24 23:11:15 +0900

    feat: nodes
    Signed-off-by: rokamu623 <r.okamura.061@ms.saitama-u.ac.jp>

---
+++ HowToDAGScheduling/DAG.cpp
@@ -0,0 +1,30 @@
+﻿#include "stdafx.h"
+#include "DAG.h"
+
+Node::Node(int idx, int wcet, Point pos)
+{
+	_idx = idx;
+	_wcet = wcet;
+	_pos = Point(64, 48) * pos;
+	_body = Circle(16);
+	_font = Font(16);
+}
+
+void Node::draw(Point pos)
+{
+	_body.movedBy(_pos+Point(16, 16)+pos).draw(Palette::Gray);
+	_body.movedBy(_pos+Point(16, 16)+pos).drawFrame(1, Palette::Black);
+	_font(Format(_wcet)).drawAt(_pos+Point(16, 16)+pos);
+}
+
+DAG::DAG(Array<Node> nodes, Array<Array<int>> edges, Point pos)
+{
+	_nodes = nodes;
+	_pos = pos;
+}
+
+void DAG::draw()
+{
+	for (auto& node : _nodes)
+		node.draw(_pos);
+}
---
+++ HowToDAGScheduling/DAG.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+class Node
+{
+private:
+	int _idx;
+	int _wcet;
+	Point _pos;
+	Circle _body;
+	Font _font;
+public:
+	Node() {};
+	Node(int idx, int wcet, Point pos);
+
+	void draw(Point pos);
+
+};
+
+class DAG
+{
+private:
+	Array<Node> _nodes;
+	Point _pos;
+public:
+	DAG() {};
+	DAG(Array<Node> nodes, Array<Array<int>> edges, Point pos);
+
+	void draw();
+};
+
